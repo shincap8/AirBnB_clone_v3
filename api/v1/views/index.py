c5b10025baa695a212d68edd663103905f072a04
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-28 14:45:58 -0500

    Fixing

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=["GET"])
+@app_views.route('/status', methods=["GET"], strict_slashes=False)
 def hello():
     return jsonify({"status": "OK"})
 
